24d3cf037bed71df0777a4d933fbc768faa3eabc
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2026-04-04 20:31:23 +0200

    reuse compliance for .prettierrc.js files

--- functions/.prettierrc.js
+++ functions/.prettierrc.js
@@ -1,4 +1,13 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health LLMonFHIR- Firebase open-source project
+//
+// SPDX-FileCopyrightText: 2026 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import pkg from "@stanfordspezi/spezi-web-configurations";
+
 const { prettierConfig } = pkg;
 
 export default prettierConfig;
